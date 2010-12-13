2996afdfbd04c3f606cb2d3d0e3573f724da05b2
lalithsuresh <suresh.lalith@gmail.com>
2010-12-14 02:34:59 +0500

    Multicast tree ready

--- src/eu/emdc/streamthing/NodeWorld.java
+++ src/eu/emdc/streamthing/NodeWorld.java
@@ -1,7 +1,9 @@
 package eu.emdc.streamthing;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import peersim.core.Node;
 
@@ -10,42 +12,70 @@ import peersim.core.Node;
  *
  */
 public class NodeWorld {
+	
+	public static Integer MAX_CHILDREN = 5;
 
-	private List<Integer> m_children; 
-	private Integer m_parent;
-	private Integer m_source;
+	private Map<Integer, Integer> m_parentMap = new HashMap<Integer, Integer> ();
+	private Map<Integer, List<Integer> > m_childrenMap = new HashMap<Integer, List<Integer> > ();
+	private List<Integer> m_listOfBuckets = new ArrayList<Integer>();
+	private Integer m_videoStreamId;
+	private Integer m_sourceNodeStreamId;
 	
-	public NodeWorld() {
-		m_children = new ArrayList<Integer>();
-	}
-	
-	public void pingChildren(Integer src) {
-		for (Integer child : m_children) {
-			/* or do this the other way around and keep a Map<Node, int> with last time pinged */
+	public NodeWorld (int videoStreamId, int sourceNodeStreamId)
+	{
+		m_videoStreamId = videoStreamId;
+		m_sourceNodeStreamId = sourceNodeStreamId;
+		List<Integer> temp = new ArrayList<Integer> ();
+		m_childrenMap.put(m_sourceNodeStreamId, temp);
+		m_parentMap.put (m_sourceNodeStreamId, -1);
+		
+		for (int i = 0; i < MAX_CHILDREN; i++)
+		{
+			m_listOfBuckets.add (sourceNodeStreamId);
 		}
 	}
 	
-	public void setParent(Integer node) {
-		m_parent = node;
+	public void AddNode (int newNodeStreamId)
+	{
+		// Iterate through MTree to find location
+		// For now, we do round robin, later, we might want to do a random join
+		int bucket = m_listOfBuckets.get(0);
+		m_parentMap.put(newNodeStreamId, bucket);
+		List<Integer> tempvect =  m_childrenMap.get (bucket);
+		tempvect.add(newNodeStreamId);
+		m_childrenMap.put (bucket, tempvect);
+		m_listOfBuckets.remove(0);
 	}
 	
-	public Integer getParent() {
-		return m_parent;
+	public void RemoveNode (int nodeToBeRemovedStreamId)
+	{
+		System.err.println("Error: Not handling node removal case");
+		/*
+		int parent = m_parentMap.get (nodeToBeRemovedStreamId);
+		List<Integer> tempvect = m_childrenMap.get (parent);
+		tempvect.remove (nodeToBeRemovedStreamId);
+		m_childrenMap.put(parent, tempvect);
+		m_parentMap.remove (nodeToBeRemovedStreamId);
+		
+		// Need to handle it's children as well
+		List<Integer>  childvect = m_childrenMap.get (nodeToBeRemovedStreamId);
+		// Remove all bucket entries
+		for (int i =0; i < m_listOfBuckets.size(); i++)
+		{
+			if (m_listOfBuckets.get(i) == nodeToBeRemovedStreamId)
+			{
+				
+			}
+		}*/
 	}
 	
-	public void setSource(Integer node) {
-		m_source = node;
+	public List<Integer> GetChildren (int queryingNodeStreamId)
+	{
+		return m_childrenMap.get (queryingNodeStreamId);
 	}
 	
-	public Integer getSource() {
-		return m_source;
-	}
-	
-	public void addChild(Integer node) {
-		m_children.add(node);
-	}
-	
-	public List<Integer> getChildren() {
-		return m_children;
+	public int GetParent (int queryingNodeStreamId)
+	{
+		return m_parentMap.get(queryingNodeStreamId);
 	}
 }
